2408bb6c5de596f4175384ccf473eb399b460207
menth0s <marslite@pm.me>
2023-08-16 05:17:09 -0400

    Implemented feeding for Finch

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Finch
+from .models import Finch, Feeding
 admin.site.register(Finch)
+admin.site.register(Feeding)
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
+from .forms import FeedingForm
 
 
 # finches = [
@@ -25,7 +26,17 @@ def finch_index(request):
 
 def finch_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
-    return render(request, 'finches/detail.html', {'finch': finch})
+    feeding_form = FeedingForm()
+
+    return render(request, 'finches/detail.html', {
+        'finch': finch,
+        'feeding_form' : feeding_form
+        })
+
+
+def add_feeding():
+    pass
+
 
 
 class FinchCreate(CreateView):
